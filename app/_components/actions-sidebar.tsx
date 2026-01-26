7fd11d5e6afec78801cac09582f726d89f304499
Abraham Bishop <crypticcodetechnologies@gmail.com>
2026-01-26 19:47:36 +0100

    feat: Add `Switch` and `Label` UI components, refactor `ActionsSidebar` for platform distribution with new logos, and remove the `icon.tsx` demo.

--- app/_components/actions-sidebar.tsx
+++ app/_components/actions-sidebar.tsx
@@ -1,4 +1,3 @@
-import { Input, InputWrapper } from "@/components/ui/input";
 import {
   Sidebar,
   SidebarContent,
@@ -8,83 +7,60 @@ import {
   SidebarGroupLabel,
   SidebarGroupContent,
 } from "@/components/ui/sidebar";
-import {
-  Collapsible,
-  CollapsibleTrigger,
-  CollapsibleContent,
-} from "@/components/ui/collapsible";
-import { cn } from "@/lib/utils";
 
-import { FaPlus } from "react-icons/fa6";
-import { ChevronDown } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
 
-const drafts = [
-  {
-    title: "The architecture behind facebook handling millions of users",
-    lastEdited: "5 mins",
-  },
-  {
-    title:
-      "How react renders a component and updates the DOM with it's virtual DOM",
-    lastEdited: "12 mins",
-  },
-];
+
+const platforms = [
+    {
+        name: "dev.to",
+        logo: "/dev.to.png",
+        connected: false,
+    },
+    {
+        name: "medium",
+        logo: "/medium.png",
+        connected: true,
+    },
+
+    {
+        name: "hashnode",
+        logo: "/hashnode.png",
+        connected: false,
+    },
+]
 
 export function ActionsSidebar() {
   return (
-    <Sidebar variant="sidebar" className="relative border-l border-r-0 border-grey-border">
+    <Sidebar variant="sidebar" className="relative border-l border-r-0 border-grey-border w-[350px]">
       <SidebarHeader />
       <SidebarContent className="bg-transparent!">
         {/* Quick Capture */}
         <SidebarGroup className="border-b border-grey-border pb-4">
           <SidebarGroupLabel className="uppercase">
-            Quick Capture
+            Distrubution
           </SidebarGroupLabel>
           <SidebarGroupContent>
-            <InputWrapper className="bg-card-bg border-grey-border mt-1 w-full">
-              <Input
-                className="text-white!"
-                placeholder="Caputure an Idea..."
-              />
-              <FaPlus className="hover:text-white cursor-pointer" />
-            </InputWrapper>
+            <div className="flex flex-col gap-2 mt-1 p-2">
+            {platforms.map((platform) => (
+                <div key={platform.name} className="w-full flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                        <div className="w-12 h-12 overflow-hidden rounded-lg">
+                            <img src={platform.logo} alt={platform.name} className="w-full h-full object-contain" />
+                        </div>
+                        <div>
+                             <p className="text-white capitalize font-semibold">{platform.name}</p>
+                             <p className="text-xs text-gray-400">{platform.connected ? "Connected" : "Not Connected"}</p>
+                        </div>
+                    </div>
+                    <Switch className="bg-primary!" defaultChecked={platform.connected} />
+                </div>
+            ))}
+            </div>
           </SidebarGroupContent>
         </SidebarGroup>
         {/*  */}
-
-        {/* Drafts */}
-        <Collapsible defaultOpen className="">
-          <SidebarGroup>
-            <SidebarGroupLabel asChild className="uppercase">
-              <CollapsibleTrigger className="w-full flex items-center justify-between">
-                Drafts
-                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-              </CollapsibleTrigger>
-            </SidebarGroupLabel>
-            <SidebarGroupContent>
-              <CollapsibleContent>
-              <div className="flex flex-col gap-2 mt-1">
-                {drafts.map((draft, i) => (
-                  <div
-                    className={cn(
-                      "w-full p-2 rounded-lg h-16 flex flex-col justify-center",
-                      i === 0 && "bg-[#18171A] border border-grey-border",
-                    )}
-                  >
-                    <h2 className="w-full truncate text-gray-200">
-                      {draft.title}
-                    </h2>
-                    <p className="text-xs text-gray-400">
-                      Edited {draft.lastEdited} ago
-                    </p>
-                  </div>
-                ))}
-              </div>
-              </CollapsibleContent>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </Collapsible>
-        {/*  */}
+        
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
